f991926ee1334c52f69e1cceb8f909b5b316f2ab
Supriya Rani <supriya.rani@hotmail.com>
2021-03-06 04:56:25 +0530

    Update ml2-project.py

--- ml2-project.py
+++ ml2-project.py
@@ -6,124 +6,96 @@ import pickle
 
 
 #App config
-st.title('')
+st.title('Credit Card Defaulter Prediction System')
 #st.progress(progress_variable_1_to_100)
 #Sidebar
 
-df=st.sidebar.text_input('File')
-
-try:
-    with open(df, "r+") as data:
-       data = pd.DataFrame(data)
-except FileNotFoundError:
-    st.error('File not found.')
-
-page_select = st.sidebar.selectbox("Modules",("Introduction", "EDA","Modeling","Prdiction","Insights"))
-if page_select == "Modeling":
-      sampling_tech=st.sidebar.selectbox("Sampling Techniques",("Randon", "Stratified","SMOTE"))
-      if sampling_tech=="Randon":
-            rand=st.sidebar.slider('Sample', min_value=0, max_value=1000)
-      elif sampling_tech=="SMOTE":
-            smt=st.sidebar.select_slider('Random State', options=[1,'2'])
-
-
-st.sidebar.button('RUN')
-HtmlFile = open(r'E:\MTech\ML 2\profile_report.html', 'rb')
-source_code = HtmlFile.read() 
-
 # loading the trained model classifier is the model name
-pickle_in_dt_tunned_classifier = open('C:\Users\KASISH\Documents\ML2 Projects\dt_tunned_classifier.pkl', 'rb')
-pickle_in_gnb_classifier = open('C:\Users\KASISH\Documents\ML2 Projects\gnb_classifier.pkl', 'rb')
-pickle_in_knn_tunned_classifier = open('C:\Users\KASISH\Documents\ML2 Projects\knn_tunned_classifier.pkl', 'rb')
-pickle_in_logit_classifier = open('C:\Users\KASISH\Documents\ML2 Projects\logit_classifier.pkl', 'rb')
-pickle_in_ml2_classifier = open('C:\Users\KASISH\Documents\ML2 Projects\ml2_classifier.pkl', 'rb')
-pickle_in_dt_stack_model_classifier = open('C:\Users\KASISH\Documents\ML2 Projects\stack_model_classifier_updated.pkl', 'rb')
+pickle_in_dt_tunned_classifier = open(r'C:\Users\KASISH\Documents\ML2 Projects\dt_tuned_classifier.pkl', 'rb')
+pickle_in_gnb_classifier = open(r'C:\Users\KASISH\Documents\ML2 Projects\gnb_classifier.pkl', 'rb')
+pickle_in_knn_tunned_classifier = open(r'C:\Users\KASISH\Documents\ML2 Projects\knn_tuned_classifier.pkl', 'rb')
+pickle_in_logit_classifier = open(r'C:\Users\KASISH\Documents\ML2 Projects\logit_classifier.pkl', 'rb')
+pickle_in_dt_stack_model_classifier = open(r'C:\Users\KASISH\Documents\ML2 Projects\stack_model_classifier_updated.pkl')
 
-classifier = pickle.load(pickle_in)
- 
+model_pkl=st.selectbox("Models",("DT Tunned", "GNB","KNN","Logit","Stack"))
+
 @st.cache()
   
 # defining the function which will make the prediction using the data which the user inputs 
-def prediction(Gender, Married, Education, Last_payment_day, Last_bill_amount):   
+def prediction(EDUCATION,SEX,MARRIAGE, PAY_0,PAY_2,PAY_3,PAY_4,PAY_5,PAY_6,PAY_AMT_1):   
  
     # Pre-processing user input    
-    if Gender == "Male":
+    if SEX == "Male":
         SEX = 1
     else:
         SEX = 2
  
-    if Married == "Unmarried":
+    if MARRIAGE == "Married":
         MARRIAGE = 1
-    elif Married == "Married":  
+    elif MARRIAGE == "Single":  
         MARRIAGE = 2
     else:
         MARRIAGE = 3
         
-    if Education == "Graduate School":
+    if EDUCATION == "Graduate School":
         EDUCATION = 1
-    elif Education == "University":  
+    elif EDUCATION == "University":  
         EDUCATION = 2
-    elif Education == "High School":
+    elif EDUCATION == "High School":
         EDUCATION = 3    
     else:
         EDUCATION = 4    
  
     
-    PAY_0 = Last_payment_day
-    BILL_AMT1 = Last_bill_amount
-  
- 
+    #PAY_0 = Last_payment_day
+    #PAY_AMT_1 = Last_bill_amount
+    classifier = pickle.load(pickle_in_dt_tunned_classifier)
+    if model_pkl == "DT Tunned":
+      classifier = pickle.load(pickle_in_dt_tunned_classifier)
+      if model_pkl =="GNB":
+            classifier = pickle.load(pickle_in_gnb_classifier)
+      elif model_pkl=="KNN":
+            classifier = pickle.load(pickle_in_knn_tunned_classifier)
+      elif model_pkl=="Logit":
+            classifier = pickle.load(pickle_in_logit_classifier)
+      elif model_pkl=="Stack":
+            classifier = pickle.load(pickle_in_dt_stack_model_classifier)
+
     # Making predictions 
     prediction = classifier.predict( 
-        [[SEX, MARRIAGE, EDUCATION, PAY_0, BILL_AMT1]])
+        [[EDUCATION,SEX,MARRIAGE,PAY_0,PAY_2,PAY_3,PAY_4,PAY_5,PAY_6,PAY_AMT_1]])
      
     if prediction == 0:
-        pred = 'not A Defaulter'
+        pred = 'not be a defaulter'
     else:
-        pred = 'a Defaulter'
+        pred = 'be a defaulter'
     return pred
       
   
 # this is the main function in which we define our webpage  
 def main(): 
-    if page_select =="Prdiction": 
-    	# front end elements of the web page 
-    	html_temp = """ 
-    	<div style ="background-color:white;padding:13px"> 
-    	<h1 style ="color:black;text-align:center;">Credit Card Defaulter Prediction</h1> 
-    	</div> 
-    	"""
+     # front end elements of the web page 
       
-# display the front end aspect
-    	#st.markdown(html_temp, unsafe_allow_html = True)
-	#model_pkl=st.selectbox("Models",("DT Tunned", "GNB","KNN","Logit","Stack"))
-	#following lines create boxes in which user can enter data required to make prediction 
-    	#Education = st.selectbox("Applicants Education",("Graduate School","University","High School","Others"))
-        #Gender = st.selectbox('Gender',("Male","Female"))
-        Married = st.selectbox('Marital Status',("Unmarried","Married","Others")) 
-    	PAY_0 = st.text("First Payment",("Yes","No"))
-    	PAY_1 = st.selectbox("Second Payment",("Yes","No"))
-    	PAY_2 = st.selectbox("Third Payment",("Yes","No"))
-    	PAY_3 = st.selectbox("Fourth Payment",("Yes","No"))
-    	PAY_4 = st.selectbox("Fifth Payment",("Yes","No"))
-    	PAY_5 = st.selectbox("Sixth Payment",("Yes","No"))
-    	PAY_6 = st.selectbox("Seventh Payment",("Yes","No"))
-    	Last_bill_amount = st.number_input("Last Pay Amount")
-    	result =""
+     # display the front end aspect
+     
+     #following lines create boxes in which user can enter data required to make prediction 
+     EDUCATION = st.selectbox('Custommer Highest Education',("Graduate School","University","High School","Others"))
+     SEX = st.selectbox('Gender',("Male","Female"))
+     MARRIAGE = st.selectbox('Marital Status',("Married","Single","Others")) 
+     PAY_0 = st.select_slider('First Payment', options=[-2,-1,0,1,2])
+     PAY_2 = st.select_slider('Second Payment', options=[-2,-1,0,1,2])
+     PAY_3 = st.select_slider('Third Payment', options=[-2,-1,0,1,2])
+     PAY_4 = st.select_slider('Fourth Payment', options=[-2,-1,0,1,2])
+     PAY_5 = st.select_slider('Fifth Payment', options=[-2,-1,0,1,2])
+     PAY_6 = st.select_slider('Sixth Payment', options=[-2,-1,0,1,2])
+     PAY_AMT_1 = st.number_input("Pay Amount")
+     result =""
       
-    	# when 'Predict' is clicked, make the prediction and store it 
-    	if st.button("Predict"): 
-        	result = prediction(Education,Gender, Married, PAY_0,PAY_1,PAY_2,PAY_3,PAY_4,PAY_5,PAY_6,Last_bill_amount) 
-        	st.success('Customer is {}'.format(result))
-    elif page_select == "EDA":
-    	print(source_code)
-    	components.html(source_code, height = 1200)
-    elif page_select == "Introduction":
-        #pr = ProfileReport(data, explorative=True)
-        #pr.to_file('E:\MTech\ML 2\profile_report.html')
-        #st_profile_report(pr)
-        st.write(data)
-
+     # when 'Predict' is clicked, make the prediction and store it  
+     if st.button("Predict"):
+          result = prediction(EDUCATION,SEX,MARRIAGE,PAY_0,PAY_2,PAY_3,PAY_4,PAY_5,PAY_6,PAY_AMT_1) 
+          st.success('This customer may {}'.format(result))
+          print(PAY_AMT_1)
 
 if __name__=='__main__': 
     main()
